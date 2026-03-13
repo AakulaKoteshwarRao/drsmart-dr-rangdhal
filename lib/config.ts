eeb4a531c3aaab2a3ccb72992fbb7f39f5d3c10c
Koteshwar Rao <kr@drsmart.in>
2026-03-13 14:32:49 +0530

    fix: use unknown cast in config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -2,5 +2,5 @@ import type { ClinicConfig } from './types'
 import defaultConfig from '../data/default.json'
 
 export function getConfig(): ClinicConfig {
-  return defaultConfig as ClinicConfig
+  return defaultConfig as unknown as ClinicConfig
 }
