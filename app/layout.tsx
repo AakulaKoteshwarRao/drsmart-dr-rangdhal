9d018bc1db22e917e0e05f079e029f06d5563dc9
Koteshwar Rao <kr@drsmart.in>
2026-03-17 11:02:29 +0530

    perf: move Google Fonts from CSS @import to <head> preconnect - eliminates render blocking

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,600;1,700&display=swap" rel="stylesheet" />
         <SchemaMarkup graphs={[coreSchemas]} />
       </head>
       <body style={cssVars}>{children}</body>
